7b58b9fdbbc61607658cdc1e3e2402c97369ff4e
cherjios <slopez0286@gmail.com>
2020-08-10 23:33:35 -0700

    Adding img to body

--- practiceapp/src/App.js
+++ practiceapp/src/App.js
@@ -1,41 +1,71 @@
 import React, { Component } from "react";
+import "./style.css";
 import Wrapper from "./components/Wrapper"
 import Footer from "./components/Footer"
 import ImgCard from "./components/ImgCard"
 import img from "./images.json"
 
+function randomNumber(){
+  let min = Math.ceil(1);
+  let max = Math.floor(18);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomImgObj () {
+  var newImgObj = {};
+
+  for(var i=0; i < 18; i++){
+   let  number = randomNumber();
+    newImgObj[i] = img[number];
+  }
+  return newImgObj;
+
+}
+var RanNewObj ={};
+
+RanNewObj = randomImgObj();
 
 
 
 class App extends Component {
 
+  
+  
   state = {
-    imageToClick: img
+    imageToClick: img,
+    score:0
   };
+
+
+
+  incremenScore() {
+    this.setState((state) => {
+      // Important: read `state` instead of `this.state` when updating.
+      return { count: state.score + 1 }
+    });
+  }
 
 
   render() {
     return (
       <div>
         <Wrapper>
-          <nav class="navbar">
+          <nav className="navbar">
             <ul>
-              <li class="brand">
+              <li className="brand">
                 <a href="/">Clicky Game</a>
               </li>
-              <li class="">Click an image to begin!</li>
-              <li>Score: 0 | Top Score: 0</li>
+              <li className="">Click an image to begin!</li>
+              <li>Score: {this.score} | Top Score: 0</li>
             </ul>
           </nav>
-
           {this.state.imageToClick.map(images => (
             <ImgCard
-              id={images.id}
-              image={images.image}
-            />
-
+            id={images.id}
+            key = {images.id}
+            image={images.image}
+          />
           ))}
-        
           <Footer />
         </Wrapper>
 
--- practiceapp/src/components/ImgCard/index.js
+++ practiceapp/src/components/ImgCard/index.js
@@ -3,7 +3,8 @@ import "./style.css";
 
 function ImgCard (props){
     return(
-             <img alt={props.id} src={props.image}  className="img-thumbnail"/>
+             <img alt={props.id} src={props.image}  className="img-thumbnail" />
+             
     )
 
 } 
