02089fe56d5270dcb2ac5e0b7a4a1ee9ab7abd6a
cherjios <slopez0286@gmail.com>
2020-08-11 23:17:22 -0700

    Updates

---
+++ precache-manifest.789a43d2b59301101634fd73f4357814.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a13e4bea332178279262bc4ca53615b",
+    "url": "/18-Clicky-Game/index.html"
+  },
+  {
+    "revision": "6499e43ac45a71556e00",
+    "url": "/18-Clicky-Game/static/css/main.3364691c.chunk.css"
+  },
+  {
+    "revision": "5040e4c90c5242cb1207",
+    "url": "/18-Clicky-Game/static/js/2.5ced6871.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/18-Clicky-Game/static/js/2.5ced6871.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6499e43ac45a71556e00",
+    "url": "/18-Clicky-Game/static/js/main.04b49402.chunk.js"
+  },
+  {
+    "revision": "e86681182ae785823c22",
+    "url": "/18-Clicky-Game/static/js/runtime-main.12f3eedd.js"
+  }
+]);
